2e01a8f863d0f074c1e37835f7bd61eea0c3d500
Cristi Rusnac <c.rusnac@student.fontys.nl>
2022-06-16 16:08:03 +0200

    modified orders page

--- EShop/client/pages/orders/index.js
+++ EShop/client/pages/orders/index.js
@@ -1,14 +1,36 @@
 const OrderIndex = ({ orders }) => {
     return (
         <div>
-            <ul>
+            <table className="table my-5">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>Order Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {orders.map(order => {
+                        return (
+                            <tr key={order.id}>
+                                <td>{order.footwear.title}</td>
+                                <td>{order.footwear.price} €</td>
+                                <td>{order.status}</td>
+                            </tr>
+                        )
+                    })
+                    }
+                </tbody>
+            </table>
+
+            {/* <ul>
                 {orders.map(order => {
                     return <li key={order.id}>
                         {order.footwear.title}, Price: {order.footwear.price} - {order.status}
                     </li>
                 })}
-            </ul>
-        </div>
+            </ul> */}
+        </div >
     )
 }
 
